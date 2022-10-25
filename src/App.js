7e8575de3f1ce9e82e96dff542887e7ada69b3ac
kennyFRNK <kennethrfrankjr@gmail.com>
2022-10-25 08:48:11 -0500

    Full site layout plus semantic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import _ from 'lodash'
+import { Grid, Image, Button} from 'semantic-ui-react'
+
 
 function App() {
   return (
     <div className="App">
+      <Grid columns ={3} centered>
+        <Grid.Row className="navbar">
+
+          <Grid.Column className="navbarColumn" floated="left">
+              <Image className="NclyneLogo" src={logo}></Image>
+          </Grid.Column>
+          <Grid.Column className="navbarColumn" floated="right">
+            <h4 className="tagline">
+            Helping creators take the next step.
+            </h4>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row >
+              <Grid.Column className="theButtons">
+                <div className="blackButton" onClick="https://medium.com/nclyne">MEDIA</div>
+              </Grid.Column>
+              <Grid.Column className="theButtons">
+              <div className="blackButton" onClick="https://medium.com/nclyne">WORK</div>
+              </Grid.Column>
+              <Grid.Column className="theButtons">
+              <div className="blackButton" onClick="https://medium.com/nclyne">ABOUT</div>
+              </Grid.Column>
+        </Grid.Row>
+      </Grid>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src=App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +45,10 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Click Here</Button>
       </header>
+
+      
     </div>
   );
 }
