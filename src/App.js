7eddd2cc3f6e6ff9a552ea9ecfa92f6e30b0a66a
kennyFRNK <kennethrfrankjr@gmail.com>
2022-10-26 12:54:15 -0500

    Introduced Slick-Slider Got rid of App Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Component } from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import _ from 'lodash'
+import Slider from 'react-slick'
 import { Grid, Image, Button} from 'semantic-ui-react'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+
 
 
 class App extends Component{
@@ -12,8 +17,17 @@ class App extends Component{
     super();
 
     this.state ={
-      buttons: ["Media","Artists", "About"]
+      buttons: ["Media","Artists", "About"],
+      settings: {
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false
+      }
     };
+
+    console.log(this.state.settings)
+
   }
 
 
@@ -22,13 +36,13 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <Grid columns ={3} centered>
+        <Grid centered={true}>
           <Grid.Row className="navbar">
   
-            <Grid.Column className="navbarColumn" floated="left">
+            <Grid.Column width="8"  className="navbarColumn" floated="left">
                 <Image className="NclyneLogo" src={logo}></Image>
             </Grid.Column>
-            <Grid.Column className="navbarColumn" floated="right">
+            <Grid.Column width="8" className="navbarColumn" floated="right">
               <h4 className="tagline">
               Helping creators take the next step.
               </h4>
@@ -37,16 +51,27 @@ class App extends Component{
           <Grid.Row >
             {this.state.buttons.map((button)=>{
               return(
-                <Grid.Column className="theButtons">
-                  <div className="blackButton" onClick="https://medium.com/nclyne">{button}</div>
+                <Grid.Column width="5" key={button} className="theButtons">
+                  <div className="blackButton">{button}</div>
                 </Grid.Column>
               );
             })}
           </Grid.Row>
+          <Grid.Row>
+            <Grid.Column className="slickSlider" width="16">
+               
+              <Slider {...this.state.settings}>
+                <div key="1" className='goldBG'>
+                </div>
+                <div key="2" className='goldBG'>
+                  <img src={logo} height={"50px"}/>
+                </div>
+
+              </Slider>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
-        <header className="App-header">
-  
-        </header>
+        
   
         
       </div>
